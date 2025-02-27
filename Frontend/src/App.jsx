c760bb9d6cb0a4fbbeaebc06ed69371108ae3560
abelmekonen <abelmekonen247@gmail.com>
2025-02-27 21:19:05 +0300

    corrected and improved the authentication and models of the flask app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import Home from './pages/Home'
+// import Login from './pages/Login'
+import SignUp from './pages/Forms/SignUp';
+// import Dashboard from './pages/Dashboard'
+// import PrivateRoute from './components/PrivateRoute'
+// import { ProvideAuth } from './hooks/useAuth'
+// import { ProvideUser } from './hooks/useUser'
+// import { ProvideWallet } from './hooks/useWallet'
+// import { ProvideTransactions } from './hooks/useTransactions'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} /> */}
+        <Route path="/signup" element={<SignUp />} />
+        {/* <PrivateRoute path="/dashboard" element={<Dashboard />} /> */}
+      </Routes>
+    </Router>
   )
 }
 
