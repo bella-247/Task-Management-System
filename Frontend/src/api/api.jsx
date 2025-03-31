bf0d3326f291b4f174499c6958350216ea2362ed
abelmekonen <abelmekonen247@gmail.com>
2025-03-31 23:59:50 +0300

    preparing for deployement

--- Frontend/src/api/api.jsx
+++ Frontend/src/api/api.jsx
@@ -1,5 +1,7 @@
 import axios from "axios"
 const API_URL = "http://127.0.0.1:5000"
+// const API_URL = "https://task-management-api.onrender.com"
+
 
 const api = axios.create({
     baseURL : API_URL
